f6a5b223e846ef6c417534ae7fd272a2419d36b7
yjs06070 <yjs06070@gmail.com>
2020-05-04 10:40:17 +0900

    initial commit

--- public/mid4/mid4.js
+++ public/mid4/mid4.js
@@ -1,9 +1,20 @@
 function sum(a,b){
-    return a+b;
-}
-function test_sum(a){
-    for(let i=0;i<5;++i){
-        a[i]=Math.floor(Math.random());
+    for(let i=0;i<a.length;++i){
+        b=a[i]+b;
     }
+    return b;
+}
+function test_sum(){
+    let a=[];
+    for(let i=0;i<5;++i){
+        a[i]=Math.floor(Math.random()*(10-1+1)+1);
+    }
+    console.log(a);
     return a;
 }
+let t=test_sum();
+console.log(t);
+console.log(t);
+console.log(t);
+console.log(t);
+console.log(t);
