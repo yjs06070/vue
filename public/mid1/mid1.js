009a665353d9e46e6181f4a18c629d86d389e844
yjs06070 <yjs06070@gmail.com>
2020-05-04 11:47:05 +0900

    initial commit

--- public/mid1/mid1.js
+++ public/mid1/mid1.js
@@ -3,7 +3,7 @@ let Max=100;
 let Min=1;
 let b=0;
 for(let i=0;i<100;++i){
-    a[i]=Math.random()*Max+Min;
+    a[i]=Math.random()*(Max-Min+1)+Min;
     b=a[i]+b;
 }
 console.log((b/100).tofixed(1));
--- public/mid2/mid2.js
+++ public/mid2/mid2.js
@@ -2,7 +2,7 @@ let a=[];
 let Max=100;
 let Min=1;
 for(let i=0;i<100;++i){
-    a[i]=Math.floor(Math.random()*Max+Min);
+    a[i]=Math.floor(Math.random()*(Max-Min+1)+Min);
     a.reverse();
 }
 
