da43457695bafa5a29d4290299eff96f9f696bc3
yjs06070 <yjs06070@gmail.com>
2020-05-04 10:20:23 +0900

    initial commit

--- public/mid3/mid3.js
+++ public/mid3/mid3.js
@@ -1,9 +1,8 @@
 let persons1=[];
 for(let i=0;i<3;++i){
-    let info={name: "홍길동", age:16+i};
-    Object.assign(persons1,info);
+    persons1.push({name: "홍길동", age:16+i})
 }
-let persons2=persons1.slice(0);
-persons1[0].age=20;
+let persons2=Object.assign({ }, persons1);
+persons1[0].age = 20;
 console.log(persons1);
 console.log(persons2);
---
+++ public/mid4/mid4.js
@@ -0,0 +1,9 @@
+function sum(a,b){
+    return a+b;
+}
+function test_sum(a){
+    for(let i=0;i<5;++i){
+        a[i]=Math.floor(Math.random());
+    }
+    return a;
+}
